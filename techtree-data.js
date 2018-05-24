fc84b0c8271cdea7ee7ba5d360053e33235d7433
aibim <33698534+aibim@users.noreply.github.com>
2018-05-24 15:33:05 +0200

    inserted myty roadmap 2018

--- techtree-data.js
+++ techtree-data.js
@@ -12,60 +12,136 @@ var bonuses = {
             "title": "Neue Funktionen",
     },
     "standards":{
-            "title":"Standardisierung",
+            "title": "Standardisierung",
     },
     "redundancy":{
             "title": "Ausfallsicherheit",
-    },
-    "user-experience":{
-            "title": "Bessere Nutzererfahrung",
     },
     "growth":{
             "title": "Wachstum"
     },
     "ux": {
-        "title": "Besseres Nutzererlebnis",
+			"title": "Besseres Nutzererlebnis",
     },
     "money": {
-        "title": "Einnahmen",
+			"title": "Einnahmen",
     }
 };
 
 var techItems = {
-    "ci": {
-        "title": "Continues Integration",
-        "bonuses": ["stability"]
-    },
     "treats": {
         "title":"Abbau Technischer Schulden",
-        "dependsOn": ["ci"]
+        "dependsOn": ["sq","ci","cd","ut"]
     },
-    "v55": {
-            "title": "Myty 5.5",
-            "costs": 4,
-            "progress":0.99,
-            "bonuses": ["performance", "clean-code"]
+		"sq": {
+			"title": "SonarQube",
+			"bonuses": ["stability","clean-code"]
+		},
+		"ci": {
+			"title": "Continuous Integration GitLab",
+			"bonuses": ["stability"]
+		},
+		"cd": {
+			"title": "Continuous Deployment",
+			"bonuses": ["performance"]
+		},
+		"ut": {
+			"title": "Unit Testing",
+			"bonuses": ["stability"]
+		},
+	"v56": {
+        "title":"myty 5.6 (Shop)",
+        "dependsOn": ["bundles","coupons","dsgvo","article-ui"]
     },
-    "v52": {
-        "title": "Myty 5.2 Feature Freeze",
-        "progress": 0.9,
-        "dependsOn":["formmanager-widgets"]
+		"bundles": {
+			"title": "Bundles",
+			"bonuses": ["standards"]
+		},
+		"coupons": {
+			"title": "Gutscheine/Rabatte",
+			"bonuses": ["standards"]
+		},
+		"dsgvo": {
+			"title": "DSGVO",
+			"bonuses": ["features"]
+		},
+		"article-ui": {
+			"title": "Artikelverwaltung UI",
+			"bonuses": ["ux"]
+		},
+    "cms": {
+        "title": "Inhaltsverwaltung",
+        "bonuses": ["ux", "performance"],
+		"dependsOn": ["v52eol","v55","ui-ux"]
     },
-    "formmanager-widgets": {
-        "title": "Formular-Manager-Widgets",
-        "progress":0,
-        "bonuses": []
-    },
-    "ux-content": {
-        "title": "UX Inhalte bearbeiten",
-        "dependsOn": ["topics"]
-    },
-    "navigation": {
-        "title": "Neue Navigation",
-        "dependsOn": ["v55"],
-    },
-    "topics": {
-        "title": "Topics",
-        "dependsOn": ["navigation"]
-    }
+		"v52eol": {
+			"title": "Myty 5.2 EOL",
+			"progress": 0.9,
+			"costs": 1,
+			"dependsOn":["template-compat","static-assets","fm-widgets"]
+		},
+			"template-compat": {
+				"title": "Template Kompatibilitäts Funktionen",
+				"progress": 0,
+				"bonuses": []
+			},
+			"static-assets": {
+				"title": "Statische Inhalte",
+				"progress": 1,
+				"bonuses": ["features"]
+			},
+			"fm-widgets": {
+				"title": "Formular-Manager-Widgets",
+				"progress": 0.15,
+				"bonuses": ["features"]
+			},
+		"v55": {
+			"title": "Myty 5.5",
+			"costs": 1,
+			"progress":  0.5,
+			"bonuses": ["performance", "clean-code"],
+			"dependsOn": ["versioning","globaltexts-review"]
+		},
+			"versioning": {
+				"title": "Versionierung Content",
+				"bonuses": ["features"]
+			},
+			"globaltexts-review": {
+				"title": "Review globale Texte",
+				"bonuses": ["ux","performance"]
+			},
+		"ui-ux": {
+			"title": "UI/UX Inhaltsverwaltung",
+			"dependsOn": ["templates","publication"],
+			"costs": 10,
+		},
+			"templates": {
+				"title": "Vorlagenverwaltung",
+				"dependsOn": ["area-dm","snippet-dm"],
+				"bonuses": ["ux"]
+			},
+			"publication": {
+				"title": "Veröffentlichungsverwaltung",
+				"dependsOn": ["area-dm","snippet-dm"],
+				"bonuses": ["ux"]
+			},
+				"area-dm": {
+					"title": "Area Datenmodell",
+					"dependsOn": ["topics"]
+				},
+				"snippet-dm": {
+					"title": "Schnipsel Datenmodell",
+					"dependsOn": ["topics"]
+				},
+					"topics": {
+						"title": "Topics",
+						"dependsOn": ["navigation"],
+						"bonuses": ["clean-code","standards"]
+					},
+						"navigation": {
+							"title": "Navigation",
+							"dependsOn": [],
+							"bonuses": ["performance","ux","standards","clean-code"]
+						},
+				
 };
